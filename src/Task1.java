7552b21af5ef90c26e3744ee00e2656764428e21
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2023-06-20 05:20:31 +0530

    Implemented removeCustomer() (to remove un-served customers)

--- src/Task1.java
+++ src/Task1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
@@ -55,7 +56,6 @@ public class Task1 {
                         System.out.print("Insert customer's name : ");
                         String customerName = input.next();
                         queue1[elementCountOfQueue1++] = customerName;
-                        stock -= 5;
                         System.out.println(customerName + " is added to Queue 1");
                     } else {
                         System.out.println("Queue 1 is full, please try another queue!");
@@ -66,7 +66,6 @@ public class Task1 {
                         System.out.print("Insert customer's name : ");
                         String customerName = input.next();
                         queue2[elementCountOfQueue2++] = customerName;
-                        stock -= 5;
                         System.out.println(customerName + " is added to Queue 2");
                     } else {
                         System.out.println("Queue 2 is full, please try another queue!");
@@ -77,7 +76,6 @@ public class Task1 {
                         System.out.print("Insert customer's name : ");
                         String customerName = input.next();
                         queue3[elementCountOfQueue3++] = customerName;
-                        stock -= 5;
                         System.out.println(customerName + " is added to Queue 3");
                     } else {
                         System.out.println("Queue 3 is full, please try another queue!");
@@ -92,11 +90,74 @@ public class Task1 {
         }
     }
 
+    public static void removeCustomer() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Choose a queue to proceed (1, 2 or 3) : ");
+        int queueNo = input.nextInt();
+        System.out.print("Choose a customer's position : ");
+        int position = input.nextInt();
+
+        switch (queueNo) {
+            case 1:
+                if (position >= 1 && position <= elementCountOfQueue1) {
+                    String customer = queue1[position - 1];
+                    shiftElementToLeftInQueue(queue1, elementCountOfQueue1, position);
+                    elementCountOfQueue1--;
+                    System.out.println(customer + " is removed from queue 1 ");
+                } else {
+                    System.out.println("Please enter a valid position in queue1!");
+                }
+                break;
+            case 2:
+                if (position >= 1 && position <= elementCountOfQueue2) {
+                    String customer = queue2[position - 1];
+                    shiftElementToLeftInQueue(queue2, elementCountOfQueue2, position);
+                    elementCountOfQueue2--;
+                    System.out.println(customer + " is removed from queue 2 ");
+                } else {
+                    System.out.println("Please enter a valid position in queue1!");
+                }
+                break;
+            case 3:
+                if (position >= 1 && position <= elementCountOfQueue3) {
+                    String customer = queue3[position - 1];
+                    shiftElementToLeftInQueue(queue3, elementCountOfQueue3, position);
+                    elementCountOfQueue3--;
+                    System.out.println(customer + " is removed from queue 3 ");
+                } else {
+                    System.out.println("Please enter a valid position in queue1!");
+                }
+                break;
+            default:
+                System.out.println("Please insert a valid queue number!");
+        }
+    }
+
+    public static  void removeServedCustomer() {
+
+    }
+
+    // This function shifts each element in the queue to the left by assigning the value of the next element to the current element
+    // moves all the elements after the specified position one step to the left.
+    // At the end, last element of the queue is set to null
+    /* This function is shifts elements to left, the customer at the specified location
+    would be removed and the size of the queue will be reduced by 1 */
+    public static void shiftElementToLeftInQueue(String[] queue, int size, int position) {
+        for (int i = position; i < size; i++) {
+            queue[i - 1] = queue[i];
+        }
+        queue[size - 1] = null;
+    }
+
     public static void main(String[] args) {
         stock = TOTAL_BURGER_COUNT;
         queue1 = new String[TOTAL_OF_QUEUE1];
         queue2 = new String[TOTAL_OF_QUEUE2];
         queue3 = new String[TOTAL_OF_QUEUE3];
+        // Initialize array with X for the visualization
+        Arrays.fill(queue1, "X");
+        Arrays.fill(queue2, "X");
+        Arrays.fill(queue3, "X");
         // Initializing element count of arrays to 0
         elementCountOfQueue1 = 0;
         elementCountOfQueue2 = 0;
@@ -112,6 +173,10 @@ public class Task1 {
                 case "102":
                 case "ACQ":
                     addCustomer();
+                    break;
+                case "103":
+                case "RCQ":
+                    removeCustomer();
                     break;
                 case "999":
                 case "EXT":
